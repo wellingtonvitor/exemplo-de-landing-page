41a8c81e6248d6dfdf0b79027f354292b1ed320b
unknown <wellingtonvitorps@gmail.com>
2022-07-22 13:32:26 -0300

    exemplo

---
+++ script11.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+	document.querySelector('.img1').src = anything;
+}
+
+function changeCircleColor(color){
+			const circle = document.querySelector('.circle');
+			circle.style.background = color;
+		}
